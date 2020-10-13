c3b187a6069d1dd056b3ad99743b513dc7de4246
yanweiding <1356104310@qq.com>
2020-10-13 20:54:38 +0800

    输出 1000 - 2000 之间所有的闰年

---
+++ 3H/TestDemo.java
@@ -0,0 +1,20 @@
+
+public class TestDemo{
+   public static void main(String[] args) {
+ 
+      for(int year=1000;year<=2000;year++){
+         if((year%4==0)&&(year%100!=0)||(year%400==0))
+         System.out.println(year);   
+      }
+
+   }
+
+}
+
+
+
+
+
+
+
+	
