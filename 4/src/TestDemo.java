634b40c3c6c1958a2780f50dfbf3a45cae82f936
yanweiding <1356104310@qq.com>
2020-10-13 17:51:52 +0800

    测试

---
+++ 4/src/TestDemo.java
@@ -0,0 +1,19 @@
+import jdk.nashorn.internal.parser.JSONParser;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Administrator
+ * Date: 2020-10-13
+ * Time: 16:13
+ */
+
+public class TestDemo {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+        System.out.println("=======");
+        System.out.println("hello");
+    }
+}
