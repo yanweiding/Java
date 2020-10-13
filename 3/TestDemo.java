c185c7da60a92cd10ff8f3ddb6e4ee5945e5ed19
yanweiding <1356104310@qq.com>
2020-10-13 16:59:27 +0800

    测试

---
+++ 3/TestDemo.java
@@ -0,0 +1,23 @@
+
+public class TestDemo {
+public static void main(String[] args) {
+int i=5;
+int num=1;
+int fac=1;
+int sum=0;
+while(num<=i){
+	fac=fac*num;
+		num++;
+		sum=sum+fac;
+}
+	System.out.println(
+		sum);
+}
+
+
+
+
+
+
+
+	
