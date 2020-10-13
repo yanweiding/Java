df24cb049552065fb17b37aaff21b2f67c3626a3
yanweiding <69305864+yanweiding@users.noreply.github.com>
2020-10-13 21:28:23 +0800

    Update TestDemo.java

--- 3A/TestDemo.java
+++ 3A/TestDemo.java
@@ -1,6 +1,6 @@
 public class TestDemo {
     public static void main(String[] args) {
-        int i = 2;
+        int i = 1;
         int sumOdd = 0;
         while (i <= 100) {
             sumOdd = sumOdd + i;
