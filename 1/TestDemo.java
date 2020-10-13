3e6550678730a51651eca168f2723e4e6613e604
yanweiding <1356104310@qq.com>
2020-10-13 17:48:01 +0800

    测试

---
+++ 1/TestDemo.java
@@ -0,0 +1,10 @@
+public class TestDemo {
+public static void main(String[] args) {
+for (int i=0;i<args.length ;i++ ) {
+	System.out.println(args[i]);
+	
+}
+System.out.println("=======");
+	System.out.println("hello");
+}
+}
