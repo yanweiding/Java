63bc2cb246acc4821baa7a050f23a2d57372179b
yanweiding <1356104310@qq.com>
2020-10-14 09:42:43 +0800

    打印1-10数字

---
+++ 3E/src/TestDemo.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Administrator
+ * Date: 2020-10-14
+ * Time: 8:23
+ */
+public class TestDemo{
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;}
+
+    }
+}
+
