ec775c26209360dcd6a068e8d69464a08645a72e
yanweiding <1356104310@qq.com>
2020-10-15 16:28:40 +0800

    找只出现一次的数字

---
+++ 4/TestDemo.java
@@ -0,0 +1,10 @@
+public class TestDemo{
+	public static void main(String[] args) {
+		int array[]={1,2,3,2,1};
+		int a=0;
+		for(int i=0;i<5;i++){
+			a=a^array[i];
+			}
+			System.out.println(a);
+		}
+}
